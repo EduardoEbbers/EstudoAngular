2809ba7f90b014ea329b5264faa9db01b79c5f43
Carlos Ebbers <cel.ebbers@gmail.com>
2022-07-28 10:45:21 -0300

    /Models, config. model, dataSrc. e repos.

---
+++ Bindings/DataBindings/src/app/Models/models.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+
+@NgModule({
+    imports: [
+        
+    ],
+    declarations: [
+        
+    ],
+    providers: [
+        
+    ],
+    exports: [
+        
+    ]
+})
+export class ModelsModule { }
---
+++ Bindings/DataBindings/src/app/component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { Repository } from "./Models/repository.model";
+
+@Component({
+    selector: 'app',
+    templateUrl: './template.html'
+})
+export class ProductComponent {
+    repository: Repository = new Repository();
+}
