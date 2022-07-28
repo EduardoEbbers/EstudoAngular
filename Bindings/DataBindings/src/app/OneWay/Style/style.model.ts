984c3952d26b6a4494162379c5bc53a829742bfb
Carlos Ebbers <cel.ebbers@gmail.com>
2022-07-28 11:55:39 -0300

    /Models/Style e /Models/Event compnts., config modules, modified files

---
+++ Bindings/DataBindings/src/app/OneWay/Style/style.model.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { StyleComponent } from "./style.component";
+
+@NgModule({
+    declarations: [
+        StyleComponent
+    ],
+    exports: [
+        StyleComponent
+    ]
+})
+export class StyleModule { }
--- Bindings/DataBindings/src/app/OneWay/oneWay.module.ts
+++ Bindings/DataBindings/src/app/OneWay/oneWay.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
-import { AttributeModule } from './Attribute/attribute.model';
-import { ClassModule } from './Class/class.model';
-import { ProductComponent } from './component';
-import { StandardPropertyModule } from './StandardProperty/standardProperty.model';
-import { StringInterpolationModule } from './StringInterpolation/stringInterpolation.model';
+import { NgModule } from "@angular/core";
+import { AttributeModule } from "./Attribute/attribute.model";
+import { ClassModule } from "./Class/class.model";
+import { ProductComponent } from "./component";
+import { EventModule } from "./Event/event.model";
+import { StandardPropertyModule } from "./StandardProperty/standardProperty.model";
+import { StringInterpolationModule } from "./StringInterpolation/stringInterpolation.model";
+import { StyleModule } from "./Style/style.model";
+
 
 @NgModule({
     imports: [
         StandardPropertyModule,
         StringInterpolationModule,
         AttributeModule,
-        ClassModule
+        ClassModule,
+        StyleModule,
+        EventModule
     ],
     declarations: [
         ProductComponent
