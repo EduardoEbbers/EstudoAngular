1d253195990724e1e440e93d23f4b1d851da63a1
Carlos Ebbers <cel.ebbers@gmail.com>
2022-08-12 10:10:10 -0300

    estruturacao de standardProperty binding

--- Bindings/DataBindings/src/app/OneWay/StandardProperty/standardProperty.component.ts
+++ Bindings/DataBindings/src/app/OneWay/StandardProperty/standardProperty.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Product } from "src/app/Models/product.model";
 import { Repository } from "src/app/Models/repository.model";
 
 @Component({
@@ -7,4 +8,39 @@ import { Repository } from "src/app/Models/repository.model";
 })
 export class StandardPropertyComponent {
     repository: Repository = new Repository();
+
+    getProducts(): Product[] {
+        return this.repository.getProducts();
+    }
+
+    getProduct(key: number): Product {
+        return this.repository.getProduct(key);
+    }
+
+    getHref(): string {
+        return this.getProducts()
+            .length == 5 ? '/bindings/oneWay' : '/bindings/oneWay/standardProperty';
+    }
+
+    getTitle(key: number): string {
+        return this.getProduct(key).category;
+    }
+    
+
+    getId(key: number): string {
+        return this.repository.getProduct(key).name;
+    }
+
+    getDisabled(): boolean {
+        return this.getProducts()
+        .length == 5 ? true : false;
+    }
+    
+    getName(key: number): string {
+        return 'btn-' + this.getProduct(key).name;
+    }
+
+    getPlaceholder(): string {
+        return "Escreva aqui algo";
+    }
 }
--- Bindings/DataBindings/src/app/core/home/home.component.ts
+++ Bindings/DataBindings/src/app/core/home/home.component.ts
@@ -1,8 +1,13 @@
 import { Component } from "@angular/core";
+import { Repository } from "src/app/Models/repository.model";
+import { Product } from 'src/app/Models/product.model';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
-export class HomeComponent { }
+export class HomeComponent {
+
+    repository: Repository = new Repository();
+}
